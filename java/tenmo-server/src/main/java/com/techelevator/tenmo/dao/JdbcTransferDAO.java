112a9b502938ddccc6a8c93c783b3f45b4e63b6c
Carlilyn Fay <carlilynfay@gmail.com>
2021-08-21 15:19:00 -0400

    Update

--- java/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDAO.java
+++ java/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDAO.java
@@ -24,15 +24,46 @@ public class JdbcTransferDAO implements TransferDAO {
 
     @Override
     public void userTransfers(Transfers transfers){
+
+        System.out.println("ANDY DEBUG ******");
+        System.out.println("AccountTo:" + transfers.getAccountTo());
+        System.out.println("AccountFROM:" + transfers.getAccountFrom());
+
+        // call the method that we defined underneath twice to get the correct
+        // to and from account id's
+
+        int accountFrom = getAccountIdFromUserId(transfers.getAccountFrom());
+        int accountTo = getAccountIdFromUserId(transfers.getAccountTo());
+
         String sql = "INSERT INTO transfers " +
                 "(transfer_type_id, transfer_status_id, account_from, account_to, amount) " +
                 "VALUES (1, 1, ?, ?, ?)";
-        jdbcTemplate.update(sql, transfers.getAccountFrom(),
-                transfers.getAccountTo(), transfers.getTransferAmount());
+        jdbcTemplate.update(sql, accountFrom, accountTo, transfers.getTransferAmount());
 
-        /*UPDATE accounts
-            SET balance = ?
-            WHERE account_id = ? */
+        //UPDATE ACCOUNT TO
+        String sqlAccountTo = "UPDATE accounts SET balance = ? WHERE account_id = ?";
+
+        //UPDATE ACCOUNT FROM
+        String sqlAccountFrom = "UPDATE accounts SET balance = ? WHERE account_id = ?";
+
+
+       int newBalance = accountDAO.getBalance(accountFrom) - transfers.getTransferAmount()
+
+    }
+
+    public int getAccountIdFromUserId (int userId) {
+
+        // 1. make a sql that retrieves the account id given the user id;
+        String sql = "select account_id from accounts where user_id = ?";
+        SqlRowSet result = jdbcTemplate.queryForRowSet(sql, userId);
+
+        int accountId = -1;
+
+        if (result.next()) {
+            accountId = result.getInt("account_id");
+        }
+
+        return accountId;
 
     }
 
